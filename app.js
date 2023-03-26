3ead3e39e6ef05941bcc0b008e6ad394e23117bb
gorayaa66 <55662051+gorayaa66@users.noreply.github.com>
2023-03-26 20:13:24 +0500

    code clean

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const path = require('path')
-const cors = require('cors')
-require('dotenv').config({ path: './config.env' })
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-//Model require
-// const user = require('./models/userModel')
+
 const corsOptions = {
-    origin: true,
-    credentials: true, 
+  origin: true,
+  credentials: true
 };
 //Middleware
-app.use(cors(corsOptions))
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(express.json());
 
-app.use("/api",require('./routes/auth.js'))
-
+app.use('/api', require('./routes/auth.js'));
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://Fatima:2410@cluster0.tc37i7k.mongodb.net/nasa')
+mongoose.connect(process.env.MONGO_URI);
 mongoose.connection.on('connected', () => {
-    console.log("database connected");
-})
+  console.log('database connected');
+});
 mongoose.connection.on('error', () => {
-    console.log("database error occured");
-})
+  console.log('database error occured');
+});
 
-app.listen(PORT , () => {
-    console.log('http://localhost:5000/')
-})
-
+app.listen(PORT, () => {
+  console.log('http://localhost:5000/');
+});
--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,31 +1,25 @@
-const user = require('../model/userModel')
-const jwt=require('jsonwebtoken');
 
-require('dotenv').config({path:'../config.env'})
+const jwt = require('jsonwebtoken');
 
-const authenticate = async (req,res,next)=>{
-   
-    try{
-        const authHeader = req.headers['authorization'];
-            const token =  authHeader.split(' ')[1];
-            console.log(token)
-            if (token == 'undefined' || token== ' ') {
-             console.log("token is empty")
 
-            }
-            jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-                if (err) return res.sendStatus(403);
-               
-                req.user = user;
-                console.log("user token")
-                next();
-              }
-              );
 
-    }catch(err){
-   
-        res.status(500).send(" No token Provided");
+const authenticate = async (req, res, next) => {
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader.split(' ')[1];
+    console.log(token);
+    if (token == 'undefined' || token == ' ') {
+      console.log('token is empty');
     }
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) return res.sendStatus(403);
 
-}
-module.exports=authenticate;
+      req.user = user;
+      console.log('user token');
+      next();
+    });
+  } catch (err) {
+    res.status(500).send(' No token Provided');
+  }
+};
+module.exports = authenticate;
